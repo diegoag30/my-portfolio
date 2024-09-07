4459c8411811fbc043271615e65b1bf93ab41f93
diegoag30 <diego.pablo.aguilar@gmail.com>
2024-09-07 13:35:58 -0300

    feat: Added proyect cards and static images

---
+++ src/components/ProyectItemComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="mx-auto" max-width="800">
+    <v-img
+      class="align-end text-white border-md border-dashed border-opacity rounded"
+      height="400"
+      :src="img_path"
+      cover
+    >
+    </v-img>
+
+    <v-card-title class="pt-4"> {{ title }} </v-card-title>
+    <v-chip class="ma-2" color="success"> Server Status </v-chip>
+    <v-card-text>
+      <div>{{ brief }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange" text="Share"></v-btn>
+
+      <v-btn color="orange" text="Explore"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+// in <script setup>
+export default {
+  props: {
+    title: String,
+    brief: String,
+    img_path: String,
+  },
+};
+</script>
+
+<style></style>
--- src/components/ProyectPreviewComponent.vue
+++ src/components/ProyectPreviewComponent.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1>Hello World</h1>
+  <v-container>
+    <v-col>
+      <ProyectItemComponent
+        title="titulo1"
+        brief="ajsjdhfb"
+        img_path="images/cdve_1.png"
+        class="pa-2 ma-2"
+      />
+
+      <ProyectItemComponent
+        title="titulo2"
+        brief="ajsjdhfb"
+        img_path="images/quiz_game_app_1.png"
+        class="pa-2 ma-2"
+      />
+
+      <v-responsive width="100%"></v-responsive>
+
+      <ProyectItemComponent
+        title="titulo3"
+        brief="ajsjdhfb"
+        img_path="images/currency_tracker_app_1.png"
+        class="pa-2 ma-2"
+      />
+    </v-col>
+  </v-container>
 </template>
 
-<script>
-export default {
-  name: "ProyectPreviewComponent",
-};
+<script setup>
+import ProyectItemComponent from "./ProyectItemComponent.vue";
 </script>
 
 <style></style>
